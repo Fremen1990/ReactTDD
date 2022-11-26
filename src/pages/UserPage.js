ce75b4b275537e044fd375d45dc6b2775f737ecb
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-26 12:01:21 +0100

    55. Layout - User Not Found

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -8,6 +8,7 @@ class UserPage extends Component {
   state = {
     user: {},
     pendingApiCall: false,
+    failResponse: undefined,
   };
 
   async componentDidMount() {
@@ -15,24 +16,35 @@ class UserPage extends Component {
     try {
       const response = await getUserById(this.props.match.params.id);
       this.setState({ user: response.data });
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      this.setState({ failResponse: error.response.data.message });
     }
+
     this.setState({ pendingApiCall: false });
   }
 
   render() {
-    const { user, pendingApiCall } = this.state;
-    return (
-      <div data-testid="user-page">
-        {!pendingApiCall && <ProfileCard user={user} />}
-        {pendingApiCall && (
-          <Alert type="secondary" center>
-            <Spinner size="big" />}
-          </Alert>
-        )}
-      </div>
+    const { user, pendingApiCall, failResponse } = this.state;
+
+    let content = (
+      <Alert type="secondary" center>
+        <Spinner size="big" />
+      </Alert>
     );
+
+    if (!pendingApiCall) {
+      if (failResponse) {
+        content = (
+          <Alert type="danger" center>
+            {failResponse}{" "}
+          </Alert>
+        );
+      } else {
+        content = <ProfileCard user={user} />;
+      }
+    }
+
+    return <div data-testid="user-page">{content}</div>;
   }
 }
 
