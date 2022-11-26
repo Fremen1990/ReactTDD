5b9ecec704020edb8068cb2ce8d62426ace01205
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-26 11:39:24 +0100

    53. Layout & Api Request for UserPage

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -18,3 +18,7 @@ export const loadUsers = (page) => {
     params: { page, size: 3 },
   });
 };
+
+export const getUserById = (id) => {
+  return axios.get(`/api/1.0/users/${id}`);
+};
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,30 @@
-const UserPage = () => {
-  return (
-    <div data-testid="user-page">
-      <h1>User page</h1>
-    </div>
-  );
-};
+import { Component } from "react";
+import { getUserById } from "../api/apiCalls";
+import ProfileCard from "../components/ProfileCard";
+
+class UserPage extends Component {
+  state = {
+    user: {},
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await getUserById(this.props.match.params.id);
+      this.setState({ user: response.data });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div data-testid="user-page">
+        <ProfileCard user={user} />
+        {user.username}
+      </div>
+    );
+  }
+}
 
 export default UserPage;
