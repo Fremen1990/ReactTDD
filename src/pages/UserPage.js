0b206c8d59b9e49834f7ff590222befd7a078f8d
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-26 11:51:13 +0100

    54. Progress Indicator - Load User

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,27 +1,36 @@
 import { Component } from "react";
 import { getUserById } from "../api/apiCalls";
 import ProfileCard from "../components/ProfileCard";
+import Spinner from "../components/Spinner";
+import Alert from "../components/Alert";
 
 class UserPage extends Component {
   state = {
     user: {},
+    pendingApiCall: false,
   };
 
   async componentDidMount() {
+    this.setState({ pendingApiCall: true });
     try {
       const response = await getUserById(this.props.match.params.id);
       this.setState({ user: response.data });
     } catch (err) {
       console.error(err);
     }
+    this.setState({ pendingApiCall: false });
   }
 
   render() {
-    const { user } = this.state;
+    const { user, pendingApiCall } = this.state;
     return (
       <div data-testid="user-page">
-        <ProfileCard user={user} />
-        {user.username}
+        {!pendingApiCall && <ProfileCard user={user} />}
+        {pendingApiCall && (
+          <Alert type="secondary" center>
+            <Spinner size="big" />}
+          </Alert>
+        )}
       </div>
     );
   }
