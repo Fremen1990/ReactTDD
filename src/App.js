be5e05dee185672420f0c0f13d202f208ce3914a
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-27 01:35:15 +0100

    64. Component - NavBar

--- src/App.js
+++ src/App.js
@@ -3,80 +3,21 @@ import LanguageSelector from "./components/LanguageSelector";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
-import { useTranslation } from "react-i18next";
-import logo from "./assets/hoaxify.png";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AccountActivationPage from "./pages/AccountActivationPage";
 import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const { t } = useTranslation();
-
   const [auth, setAuth] = useState({
     isLoggedIn: false,
     id: "",
   });
 
-  // const [path, setPath] = useState(window.location.pathname);
-
-  // const onClickLink = (event) => {
-  //   event.preventDefault();
-  //   const path = event.currentTarget.attributes.href.value;
-  //   window.history.pushState({}, "", path);
-  //   setPath(path);
-  // };
-
   return (
     <Router>
-      <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
-        <div className="container">
-          <Link
-            className="navbar-brand"
-            to="/"
-            title="Home"
-            // onClick={onClickLink}
-          >
-            <img src={logo} alt="Hoaxify" width="60" />
-            Hoaxify
-          </Link>
-          <ul className="navbar-nav">
-            {!auth.isLoggedIn && (
-              <>
-                <Link
-                  className="nav-link"
-                  to="/signup"
-                  // onClick={onClickLink}
-                >
-                  {t("signUp")}
-                </Link>
-                <Link
-                  className="nav-link"
-                  to="/login"
-                  // onClick={onClickLink}
-                >
-                  Login
-                </Link>
-              </>
-            )}
-            {auth.isLoggedIn && (
-              <Link className="nav-link" to={`/user/${auth.id}`}>
-                My Profile
-              </Link>
-            )}
-          </ul>
-        </div>
-      </nav>
-
       <div className="container pt-3">
-        {/*// Old JS routing*/}
-        {/*{window.location.pathname === "/" && <HomePage />}*/}
-        {/*{window.location.pathname === "/signup" && <SignUpPage />}*/}
-        {/*{window.location.pathname === "/login" && <LoginPage />}*/}
-        {/*{window.location.pathname.startsWith("/user/") && <UserPage />}*/}
-        {/*{path === "/" && <HomePage />}*/}
-        {/*{path === "/signup" && <SignUpPage />}*/}
-        {/*{path === "/login" && <LoginPage />}*/}
-        {/*{path.startsWith("/user/") && <UserPage />}*/}
+        <NavBar auth={auth} />
 
         <Route exact path="/" component={HomePage} />
         <Route path="/signup" component={SignUpPage} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/hoaxify.png";
+import { useTranslation } from "react-i18next";
+
+function NavBar({ auth }) {
+  const { t } = useTranslation();
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+      <div className="container">
+        <Link className="navbar-brand" to="/" title="Home">
+          <img src={logo} alt="Hoaxify" width="60" />
+          Hoaxify
+        </Link>
+        <ul className="navbar-nav">
+          {!auth.isLoggedIn && (
+            <>
+              <Link className="nav-link" to="/signup">
+                {t("signUp")}
+              </Link>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </>
+          )}
+          {auth.isLoggedIn && (
+            <Link className="nav-link" to={`/user/${auth.id}`}>
+              My Profile
+            </Link>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
